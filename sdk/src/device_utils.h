ad047ec586d99fa659440da77f079d17c30e42e9
Santosh Madanwad <santosh.madanwad@einfochips.com>
2019-11-01 05:43:44 +0000

    Modified deinterleave code for Thor96

--- sdk/src/device_utils.h
+++ sdk/src/device_utils.h
@@ -8,67 +8,73 @@ namespace aditof {
 static void deinterleave(const char *source, uint16_t *destination,
                          size_t source_len, size_t dest_width,
                          size_t dest_height) {
-    // Check if only deinterleave is needed or unpacking+deinterleave is needed
-    if (source_len == (dest_width * dest_height * 2)) {
-        unsigned char *sPtr = (unsigned char *) source; 
-        unsigned short *depthPtr = destination; 
-        unsigned short *irPtr = destination + (dest_width * dest_height) /2;
-        int irOffset = dest_width * dest_height /2;
 
-        for (int i = 0; i < dest_height; i++) {
-            if(i&1) 
-                memcpy(irPtr + (i >> 1) * dest_width, sPtr, dest_width* 2); 
-            else
-                memcpy(depthPtr + (i >> 1) * dest_width, sPtr, dest_width * 2);
-            sPtr += dest_width * 2;
-        } 
+    size_t offset[2] = {0, dest_height * dest_width / 2};
+    size_t offset_idx = 0;
+    size_t j = 0;
+
+    // For platforms that output 16bit data per pixel, unpacking the pixel data
+    // is not needed. Only deinterleave.
+    if (source_len == dest_width*dest_height*2) {
+        unsigned short *src = (unsigned short *)source;
+        for (size_t i = 0; i < source_len; i += 2) {
+
+            offset_idx = ((j / dest_width) % 2);
+
+            destination[offset[offset_idx]] = src[i];
+            offset[offset_idx]++;
+
+            destination[offset[offset_idx]] = src[i+1];
+            offset[offset_idx]++;
+
+            j += 2;
+        }
+        return;
     }
-    else {
-        size_t offset[2] = {0, dest_height * dest_width / 2};
-        size_t offset_idx = 0;
-        size_t j = 0;
 
-        if (dest_width == 668) {
-            for (size_t i = 0; i < source_len; i += 3) {
-                if ((i != 0) & (i % (336 * 3) == 0)) {
-                    j -= 4;
-                }
-
-                destination[j] =
-                    (((unsigned short)*(((unsigned char *)source) + i)) << 4) |
-                    (((unsigned short)*(((unsigned char *)source) + i + 2)) &
-                     0x000F);
-                j++;
-
-                destination[j] =
-                    (((unsigned short)*(((unsigned char *)source) + i + 1)) << 4) |
-                    ((((unsigned short)*(((unsigned char *)source) + i + 2)) &
-                      0x00F0) >>
-                     4);
-                j++;
+    // For platforms that output 12bit data per pixel, unpacking the pixel data
+    // and deinterleaving depth & IR is needed. Do unpack and deinterleave.
+    if (dest_width == 668) {
+        for (size_t i = 0; i < source_len; i += 3) {
+            if ((i != 0) & (i % (336 * 3) == 0)) {
+                j -= 4;
             }
-        } else {
-            for (size_t i = 0; i < source_len; i += 3) {
 
-                offset_idx = ((j / dest_width) % 2);
+            destination[j] =
+                (((unsigned short)*(((unsigned char *)source) + i)) << 4) |
+                (((unsigned short)*(((unsigned char *)source) + i + 2)) &
+                 0x000F);
+            j++;
 
-                destination[offset[offset_idx]] =
-                    (((unsigned short)*(((unsigned char *)source) + i)) << 4) |
-                    (((unsigned short)*(((unsigned char *)source) + i + 2)) &
-                     0x000F);
-                offset[offset_idx]++;
+            destination[j] =
+                (((unsigned short)*(((unsigned char *)source) + i + 1)) << 4) |
+                ((((unsigned short)*(((unsigned char *)source) + i + 2)) &
+                  0x00F0) >>
+                 4);
+            j++;
+        }
+    } else {
+        for (size_t i = 0; i < source_len; i += 3) {
 
-                destination[offset[offset_idx]] =
-                    (((unsigned short)*(((unsigned char *)source) + i + 1)) << 4) |
-                    ((((unsigned short)*(((unsigned char *)source) + i + 2)) &
-                      0x00F0) >>
-                     4);
-                offset[offset_idx]++;
+            offset_idx = ((j / dest_width) % 2);
 
-                j += 2;
-            }
+            destination[offset[offset_idx]] =
+                (((unsigned short)*(((unsigned char *)source) + i)) << 4) |
+                (((unsigned short)*(((unsigned char *)source) + i + 2)) &
+                 0x000F);
+            offset[offset_idx]++;
+
+            destination[offset[offset_idx]] =
+                (((unsigned short)*(((unsigned char *)source) + i + 1)) << 4) |
+                ((((unsigned short)*(((unsigned char *)source) + i + 2)) &
+                  0x00F0) >>
+                 4);
+            offset[offset_idx]++;
+
+            j += 2;
         }
     }
+}
 } // namespace aditof
 
 #endif // DEVICE_UTILS_H
--- sdk/src/target/thor96/target_definitions.h
+++ sdk/src/target/thor96/target_definitions.h
@@ -7,5 +7,6 @@ static const char *EEPROM_DEV_PATH = "/sys/bus/i2c/devices/0-0056/eeprom";
 
 static const char *CAPTURE_DEVICE_NAME = "i.MX6S_CSI";
 
+static const char *TEMP_SENSOR_DEV_PATH = "/dev/i2c-1";
 
 #endif // TARGET_DEFINITIONS_H
